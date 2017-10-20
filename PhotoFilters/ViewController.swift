f2d408c86b4606ca8e60d3c4598c73b1f44b7cb4
Jose Carlos Montero Gómez <sipox11@gmail.com>
2017-10-21 01:39:47 +0200

    Add graphic interface and add logic to button to apply a random filter to the picture

--- PhotoFilters/ViewController.swift
+++ PhotoFilters/ViewController.swift
@@ -10,6 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+    // Create a place to render the filtered image
+    let context = CIContext(options: nil)
+    
+    @IBAction func applyFilter(_ sender: Any) {
+        
+        // Create an image to filter
+        guard let img = photoImageView.image else { return }
+        let inputImage = CIImage(image: img)
+        
+        // Create a random color to pass to a filter
+        let randomColor = [kCIInputAngleKey: (Double(arc4random_uniform(314)) / 100)]
+        
+        // Apply a filter to the image
+        let filteredImage = inputImage!.applyingFilter("CIHueAdjust", parameters: randomColor)
+        
+        // Render the filtered image
+        let renderedImage = context.createCGImage(filteredImage, from: filteredImage.extent)
+        
+        // Reflect the change back in the interface
+        photoImageView.image = UIImage(cgImage: renderedImage!)
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
