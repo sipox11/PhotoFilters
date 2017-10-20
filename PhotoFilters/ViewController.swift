09d04d12c11d692a7b7a8216dd44fb3a09faa925
Jose Carlos Montero Gómez <jcmontero@Joses-MacBook-Pro.local>
2017-10-21 00:46:16 +0200

    Init repository

---
+++ PhotoFilters/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PhotoFilters
+//
+//  Created by Jose Carlos Montero Gómez on 21/10/2017.
+//  Copyright © 2017 sipox11. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
